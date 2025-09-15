94b16d1436eda7b3ebade7f4f03e478cd886bcbb
buhiui <69589740+buhiui@users.noreply.github.com>
2025-09-15 17:39:17 +0800

    增加CORS支持

---
+++ functions/_middleware.js
@@ -0,0 +1,9 @@
+export const onRequest: PagesFunction = async (context) => {
+  const response = await context.next();
+
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+
+  return response;
+};
