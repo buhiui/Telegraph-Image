19cecd97b1a5975fba8cf30dbd922bbfa7f33880
buhiui <69589740+buhiui@users.noreply.github.com>
2025-09-15 17:40:52 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,6 +1,10 @@
-export const onRequest: PagesFunction = async (context) => {
-  const response = await context.next();
+export const onRequest = async (context) => {
+  const { request, next } = context;
 
+  // 先执行后续函数，拿到响应
+  const response = await next();
+
+  // 添加 CORS 头
   response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   response.headers.set("Access-Control-Allow-Headers", "Content-Type");
